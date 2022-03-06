7714b1177cf08681234d0103294e01df5e518992
nepp.plus <nepp.plus@gmail.com>
2022-03-06 10:30:10 +0900

    이미지뷰를 클릭하면, 새 액티비티로 이동  - 사진 크게 보는 화면

--- app/src/main/java/com/neppplus/librarypractice_20220306/MainActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_20220306/MainActivity.kt
@@ -1,11 +1,33 @@
 package com.neppplus.librarypractice_20220306
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupEvents()
+        setValues()
     }
+
+    fun setupEvents() {
+
+        imgProfile.setOnClickListener {
+
+//            사진 크게 보는 화면으로 넘어가자.
+
+            val myIntent = Intent(this, ViewPhotoActivity::class.java)
+            startActivity(myIntent)
+
+        }
+
+    }
+
+    fun setValues() {
+
+    }
+
 }
