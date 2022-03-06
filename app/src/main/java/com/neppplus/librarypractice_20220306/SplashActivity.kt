135079d60aba0f09616eed08cd0f405f9bad7116
nepp.plus <nepp.plus@gmail.com>
2022-03-06 13:34:27 +0900

    스플래쉬 화면 추가

---
+++ app/src/main/java/com/neppplus/librarypractice_20220306/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.neppplus.librarypractice_20220306
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+
+    fun setupEvents() {
+
+    }
+
+    fun setValues() {
+
+    }
+
+}
